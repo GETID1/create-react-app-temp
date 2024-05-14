bf31cf60234920b3ec67bb87118406469aa2e611
chenzhihe <chenzhihe@huya.com>
2024-05-14 23:19:54 +0800

    feat: template

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from "react";
 import "./App.css";
-import StarRating from "./components/StarRating";
-import Dialog from "./components/Dialog";
-import reactLogo from "./assets/images/logo.png";
-
 
 function App() {
-  const [isShowDialog, setIsShowDialog] = React.useState(false);
   return (
     <div className="App">
-      <div className="container">
-        <StarRating />
-        <Dialog
-          visible={isShowDialog}
-          onCancel={() => {
-            setIsShowDialog(false);
-          }}
-        >
-          <div className="dialog-children">
-            <img src={reactLogo} alt="" />
-          </div>
-        </Dialog>
-        <div className="button-wrapper">
-          <button onClick={() => {
-            setIsShowDialog(true);
-          }}>open diaglo</button>
-        </div>
-      </div>
+      <div className="container">这里写内容</div>
     </div>
   );
 }
